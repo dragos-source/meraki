7069b1e6e34bcb91deabbcbf8da982014b9e4ee1
dragos-source <69633912+dragos-source@users.noreply.github.com>
2022-09-30 09:44:40 +0200

    Update meraki_get_clients.sh

--- meraki_get_clients.sh
+++ meraki_get_clients.sh
@@ -10,7 +10,7 @@ echo 'Getting the list of Organisations you are allowed to read:'
 curl -s ${url}/api/v1/organizations -H "X-Cisco-Meraki-API-Key: ${api_key}" | jq -r ".[] |[.name, .id] | join(\",\")" | column -ts ','
 read -p 'Provide the organization ID from the list above: ' organizationId
 
-#get networks in the organisation
+#get networks in the organisation and attached clients
 curl -s ${url}/api/v1/organizations/${organizationId}/networks -H "X-Cisco-Meraki-API-Key: ${api_key}" | jq -r ".[] | [.name, .id] | join(\";\")"| while IFS=';' read -r network network_id 
     do
        curl -s -L --request GET --url ${url}/api/v1/networks/${network_id}/clients?perPage=1000  --header "X-Cisco-Meraki-API-Key: ${api_key}" | jq -r '.[] |[.mac, .description, .ip, .manufacturer, .recentDeviceName, .recentDeviceConnection, .switchport] | join(";")' | while IFS=';' read -r mac description ip namufacturer recentDeviceName recentDeviceConnection switchport
